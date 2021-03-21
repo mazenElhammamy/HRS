be3b3046d1d72ecf0410b84b9de6bc4cf9944ec5
MAZEN\Mazen <mazz.ashraff@gmail.com>
2021-03-21 13:07:42 +0200

    - update package json - update routes in app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,29 @@
 import React, { Component } from 'react';
 import Navbar from './Layout/Navbar';
 import Footer from './Layout/Footer';
-import Home from './Components/Home';
 import AddEmployee from './Components/AddEmployee';
 import { Switch, Route } from 'react-router-dom';
 import AddDepartment from './Components/AddDepartment';
 import AllDepartments from './Components/AllDepartments';
 import SignIn from './Components/SignIn';
-import EmployeeHome from './Components/EmployeeHome';
+import Home from './Components/Home';
 import AddTitle from './Components/AddTitle';
+import AllEmployee from './Components/AllEmployee';
+import EditEmployee from './Components/EditEmployee';
+import Profile from './Components/EmployeeProfile';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import EmployeeStore from './Store/employeeStore';
+
+
 
 export default class App extends Component {
 
+
+	componentDidMount(){
+		if (localStorage.getItem('token')){
+			EmployeeStore.setLoggedIn();
+		}
+	}
 	render() {
 		return (
 			<div className="App" id="page-container">
@@ -24,8 +36,11 @@ export default class App extends Component {
 						<Route exact path="/addDepartment" component={AddDepartment} />
 						<Route exact path="/addTitle" component={AddTitle} />
 						<Route exact path="/allDepartment" component={AllDepartments} />
+						<Route exact path="/allEmployees" component={AllEmployee} />
 						<Route exact path="/signIn" component={SignIn} />
-						<Route exact path="/employeeHome" component={EmployeeHome} />
+						<Route exact path="/home"  component={Home} /> 
+						<Route exact path="/editEmployee" component={EditEmployee} />
+						<Route exact path="/employeeProfile" component={Profile} />
 					</Switch>
 				</div>
 				<div id="footer" >
