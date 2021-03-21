fa116674cdc89e2a0e5d525e5ed5dd2920175050
MAZEN\Mazen <mazz.ashraff@gmail.com>
2021-03-21 12:55:43 +0200

    Removing await

--- frontend/src/Actions/DepartmentActions.js
+++ frontend/src/Actions/DepartmentActions.js
@@ -1,50 +1,51 @@
 import dispatcher from "../dispatcher/dispatcher";
 import axios from "axios"
-export async function createDepartment(department) {
+export function createDepartment(department, history) {
     const options = {
         method: 'POST',
         data: department,
         url: 'http://localhost:5000/api/department/addDepartment',
     };
-  await  axios(options)
+    axios(options)
         .then((res) => {
             dispatcher.dispatch({
-                type:"CREATE_DEPARTMENT",
-                data:res.data.departments,
+                type: "CREATE_DEPARTMENT",
+                data: res.data.departments,
+            });
+            history.push('/allDepartment');
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+
+}
+
+export function getAllDepartment() {
+    axios.get('http://localhost:5000/api/department/getAllDepartment')
+        .then((res) => {
+            dispatcher.dispatch({
+                type: "GET_ALL_DEPARTMENTS",
+                data: res.data.departments,
             });
         })
         .catch((err) => {
-           console.log(err)
+            console.log(err)
         });
-  
+
 }
 
-export async function  getAllDepartment() {
-   await axios.get('http://localhost:5000/api/department/getAllDepartment')
-        .then((res) => {
-            dispatcher.dispatch({
-                type:"GET_ALL_DEPARTMENTS",
-                data:res.data.departments,
-            });
-        })
-        .catch((err) => {
-           console.log(err)
-        });
-  
-}
-
-export async function deleteDepartment(_id) {
+export function deleteDepartment(_id) {
     const options = {
         method: 'POST',
-        data:{_id:_id} ,
+        data: { _id: _id },
         url: 'http://localhost:5000/api/department/deleteDepartment',
     };
-  await  axios(options)
+    axios(options)
         .then((res) => {
-            console.log("done",res.data)
+            console.log("done", res.data)
         })
         .catch((err) => {
-           console.log(err)
+            console.log(err)
         });
-  
+
 }
--- frontend/src/Actions/EmployeeActions.js
+++ frontend/src/Actions/EmployeeActions.js
@@ -1,20 +1,85 @@
 import dispatcher from "../dispatcher/dispatcher";
 import axios from "axios"
 
-export async function addNewEmployee(employee) {
- console.log("title from action ",employee)
-    const options = {
-        method: 'POST',
-        data: employee,
-        url: 'http://localhost:5000/api/employee/addNewEmployee',
-    };
-  await  axios(options)
-        .then((res) => {
-           console.log(res)
-        })
-        .catch((err) => {
-           console.log(err)
-        });
-  
-}
+export function addNewEmployee(employee, history) {
+  const options = {
+    method: 'POST',
+    data: employee,
+    url: 'http://localhost:5000/api/employee/addNewEmployee',
+  };
+  axios(options)
+    .then((res) => {
+      history.push('/home');
+    })
+    .catch((err) => {
+      console.log(err)
+    });
 
+}
+export function login(data, history) {
+  const options = {
+    method: 'POST',
+    data: data,
+    url: 'http://localhost:5000/api/employee/login',
+  };
+  axios(options)
+    .then((res) => {
+      console.log("resssss", res)
+      dispatcher.dispatch({
+        type: "LOGGED_IN",
+        data: res.data.token,
+        history: history
+      });
+      // localStorage.setItem('token', res.data.token);
+    })
+    .catch((err) => {
+      console.log("errrrrr", err)
+    });
+
+}
+export function editEmployee(employee, history) {
+  const options = {
+    method: 'PUT',
+    data: employee,
+    url: 'http://localhost:5000/api/employee/editEmployee',
+  };
+  axios(options)
+    .then((res) => {
+      history.push('/home');
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+}
+export function getAllEmployees() {
+  axios.get('http://localhost:5000/api/employee/getAllEmployees')
+    .then((res) => {
+      dispatcher.dispatch({
+        type: "GET_ALL_Employees",
+        data: res.data.employees,
+      });
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+
+}
+export function getAllLeaders(titleId) {
+  const options = {
+    method: 'POST',
+    data: { titleId },
+    url: 'http://localhost:5000/api/employee/findLeaders',
+  };
+  axios(options)
+    .then((res) => {
+      console.log("ressss", res.data.leaders)
+      dispatcher.dispatch({
+        type: "GET_ALL_Employees",
+        data: res.data.leaders,
+      });
+    })
+    .catch((err) => {
+      console.log("errr", err)
+    });
+}
